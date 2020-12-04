ca908c4717e0502af9344df6b186c4d20fde3fff
musa-friday <musaalayafin13@gmail.com>
2020-12-04 17:10:48 +0100

    special-musa

---
+++ AWS.py
@@ -0,0 +1,17 @@
+dict = {
+    "grace" : "lead inst dsn nigeria ",
+    "musa" : "assist lead isnst",
+    "abel" : "cheif wipe",
+    "precious" : "ekome",
+}
+
+dict["muyiwa"]= "anandes"
+dict["grace"] = 4900
+
+if "abelss" not in dict.keys():
+    dict["abelss"]= "2100"
+
+
+
+print(dict["abel"])
+print(dict.get("grace"))
